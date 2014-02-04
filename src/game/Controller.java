0ba1b260bc71d634404ac915d68d3e917b076dd8
Rasmus Greve <rasmusgreve@gmail.com>
2014-02-04 14:54:29 +0100

    Started implementation of controller

--- src/game/Controller.java
+++ src/game/Controller.java
@@ -3,13 +3,34 @@ package game;
 import player.Player;
 
 public class Controller {
-
+	private final Player[] players;
+	private GameState state;
+	private boolean gameRunning;
+	
 	public Controller(Player[] players){
-		
+		this.players = players;
+		state = new GameState();
 	}
 	
 	public void startGame()
 	{
-		
+		gameRunning = true;
+		while (gameRunning){
+			takeTurn();
+		}
+		System.out.println("Game is over");
+	}
+	
+	private void takeTurn()
+	{
+		System.out.println("Take turn");
+		java.util.Random r = new java.util.Random();
+		if (r.nextInt(10) == 0)
+			gameRunning = false;
+		/*
+		Player cur = players[state.currentPlayerTurn];
+		Answer ans = cur.PerformTurn(state);
+		state = state.apply(ans);
+		*/
 	}
 }
