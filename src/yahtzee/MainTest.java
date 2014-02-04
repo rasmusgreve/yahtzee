5217cc9aa7a3f1ac1fbe68a70dd4b368763686fc
mengsoc <asager@gmail.com>
2014-02-04 13:33:53 +0100

    amazing new change

--- src/yahtzee/MainTest.java
+++ src/yahtzee/MainTest.java
@@ -3,7 +3,6 @@ package yahtzee;
 public class MainTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Yahtzee world");
+		System.out.println("Hello SUPER Yahtzee world");
 	}
-
 }
