f52461d6802267cabaecc5334f7bab9fd16299bb
Rasmus Greve <rasmusgreve@gmail.com>
2014-02-04 15:03:14 +0100

    Implemented some input handling in human player

--- src/player/HumanPlayer.java
+++ src/player/HumanPlayer.java
@@ -1,13 +1,26 @@
 package player;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import game.Answer;
 import game.GameState;
 
 public class HumanPlayer implements Player {
 
+	private BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	
 	@Override
 	public Answer PerformTurn(GameState state) {
-		// TODO Auto-generated method stub
+		System.out.println("Your turn");
+		System.out.println("Type something:");
+		try {
+			String s = input.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
