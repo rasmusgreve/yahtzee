d132f8a9b98e34c8b7c9d35cf919b78f375951a3
torybash <torybash@gmail.com>
2014-02-06 09:33:33 +0100

    Scoreboard converting to int

--- src/game/Scoreboard.java
+++ src/game/Scoreboard.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.util.BitSet;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,13 +8,23 @@ public class Scoreboard {
 
 	HashMap<ScoreType, Integer> scoreMap = new HashMap<ScoreType, Integer>();
 	
+	
+	
+	
 	public Scoreboard(){				
 		for (ScoreType scoreType : ScoreType.values()) {
 			scoreMap.put(scoreType, -1);
 		}
+		
+		scoreMap.put(ScoreType.ONES, 4);
+		
+		scoreMap.put(ScoreType.SMALL_STRAIGHT, 25);
+		
+		scoreMap.put(ScoreType.YAHTZEE, 50);
+		
+		ConvertMapToInt();
 	}
-	
-	
+
 	public enum ScoreType{
 	    ONES, 
 	    TWOS,
@@ -21,7 +32,6 @@ public class Scoreboard {
 	    FOURS,
 	    FIVES,
 	    SIXES,
-	    BONUS,
 	    THREE_OF_A_KIND,
 	    FOUR_OF_A_KIND,
 	    FULL_HOUSE,
@@ -39,14 +49,13 @@ public class Scoreboard {
 //	    FOURS(3),
 //	    FIVES(4),
 //	    SIXES(5),
-//	    BONUS(6),
-//	    THREE_OF_A_KIND(7),
-//	    FOUR_OF_A_KIND(8),
-//	    FULL_HOUSE(9),
-//	    SMALL_STRAIGHT(10),
-//	    BIG_STRAIGHT(11),
-//	    YAHTZEE(12),
-//	    CHANCE(13);
+//	    THREE_OF_A_KIND(6),
+//	    FOUR_OF_A_KIND(7),
+//	    FULL_HOUSE(8),
+//	    SMALL_STRAIGHT(9),
+//	    BIG_STRAIGHT(10),
+//	    YAHTZEE(11),
+//	    CHANCE(12);
 //	    
 //
 //	    private final int id;
@@ -55,6 +64,69 @@ public class Scoreboard {
 //	}
 
 	
+	
+	void ConvertMapToInt(){
+		System.out.println("--------------");
+		System.out.println("CONVERTING TO INT");
+		System.out.println("--------------");
+		int upperCounter = 0;
+		int totalCounter = 0;
+		
+		
+		boolean[] scores = new boolean[13];
+		
+		
+		int i = 0;
+		for (ScoreType scoreType : ScoreType.values()) {
+			int scoreAmount = scoreMap.get(scoreType);
+			if (scoreAmount > -1){
+				scores[i] = true;
+				if (scoreType.ordinal() < 6) upperCounter += scoreAmount;
+				totalCounter += scoreAmount;
+			}
+			
+			i++;
+		}
+		
+		
+		
+		int result = upperCounter + (totalCounter << 6);
+		
+		for (int j = 0; j < scores.length; j++) {
+			int trueOrFalse = scores[j] ? 1 : 0;
+			result |= (trueOrFalse) << (15+j);
+		}		
+		
+		for (int j = 0; j < scores.length; j++) {
+			System.out.println("ScoreType: " + ScoreType.values()[j] + ", scores[j]: " + scores[j]);
+		}
+		System.out.println("upperCounter: " + upperCounter + ", totalCounter: " + totalCounter + " ,result: " + result);
+
+		ConvertIntToMap(result);
+	}
+	
+	void ConvertIntToMap(int aInt){
+		System.out.println("--------------");
+		System.out.println("CONVERTING TO MAP");
+		System.out.println("--------------");
+		int maskUpperCounter = (1<<6)-1;
+		int maskTotalCounter = ((1<<9)-1)<<6;
+		
+		int upperCounter = aInt & maskUpperCounter;
+		int totalCounter = (aInt & maskTotalCounter) >> 6;
+		boolean[] scores = new boolean[13];
+		
+		for (int i = 0; i < scores.length; i++) {
+			int maskScores = ((1<<(15+i)));
+			scores[i] = (aInt & maskScores) >> (15+i) == 1;
+			
+			System.out.println("ScoreType: " + ScoreType.values()[i] + ", scores[i]: " + scores[i]);
+		}
+		
+	
+		System.out.println("upperCounter: " + upperCounter + ", totalCounter: " + totalCounter);
+
+	}
 	
 	public void PrintScoreBoard(){
 		System.out.println("Scoreboard for player: " + "PLAYERNAMEHERE");
