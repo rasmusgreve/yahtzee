13442289f633dbb2f57a1a3266bbda57650ddac5
Rasmus Greve <rasmusgreve@gmail.com>
2014-02-07 10:54:55 +0100

    Added example of object storing/loading to single player AI

--- src/player/SinglePlayerAI.java
+++ src/player/SinglePlayerAI.java
@@ -1,20 +1,71 @@
 package player;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
 import game.Answer;
 import game.Question;
 
 public class SinglePlayerAI implements Player {
 
+	public int[] testCache = new int[100];
+	private static final String filename = "testCache.bin";
+	
 	@Override
 	public Answer PerformTurn(Question question) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	
-	public void setID(int id){
+	@Override
+	public void reset(int id){
 		
 	}
 	
+	public static void main(String[] args) {
+		java.util.Random rand = new java.util.Random();
+		SinglePlayerAI ai = new SinglePlayerAI();
+		ai.loadArray();
+		ai.testCache[rand.nextInt(ai.testCache.length)] = rand.nextInt();
+		
+		System.out.println(Arrays.toString(ai.testCache));
+		
+		ai.finalize();
+	}
+	
+	public void loadArray()
+	{
+		try {
+			FileInputStream fis = new FileInputStream(filename);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			testCache = (int[]) ois.readObject();
+			ois.close();
+			fis.close();
+		} catch (Exception e) {
+			System.out.println("WARNING! cache not loaded");
+			testCache = new int[100];
+		}
+	}
+	
+	@Override
+	public void finalize(){
+		//Save lookup table to persistent medium
+		try {
+			FileOutputStream fos = new FileOutputStream(filename);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(testCache);
+			oos.close();
+			fos.close();
+		} catch (IOException e) {
+			System.out.println("WARNING! cache not stored");
+		}
+	}
+	
+	@Override
 	public String getName()
 	{
 		return "Single player AI";
