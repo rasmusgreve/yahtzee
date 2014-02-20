0542965ea0bd31fd3b642ee9bdc8a87d43f60c2e
Rasmus Greve <rasmusgreve@gmail.com>
2014-02-20 09:41:19 +0100

    Added multiplayerAI class

---
+++ src/tests/MultiPlayerAI.java
@@ -0,0 +1,35 @@
+package tests;
+
+import game.Answer;
+import game.Question;
+import player.Player;
+
+public class MultiPlayerAI implements Player {
+
+	private int id;
+	
+	@Override
+	public Answer PerformTurn(Question question) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+	
+	@Override
+	public String getName() {
+		return "Multi player AI";
+	}
+
+	@Override
+	public void reset(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public void cleanUp() {
+		System.out.println("Multiplayer AI cleanup. Nothing to do?");
+	}
+
+}
