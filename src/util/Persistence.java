fb6dcf573593ce34c12c2aeba5af9a5d30d6d601
Rasmus Greve <rasmusgreve@gmail.com>
2014-03-13 11:42:42 +0100

    Cleanup in Persitence class. Added javadoc

--- src/util/Persistence.java
+++ src/util/Persistence.java
@@ -1,153 +1,45 @@
 package util;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.Arrays;
 
-import player.MultiPlayerAI;
-
 public class Persistence {
-
-	public static double[][] loadDoubleArray(String filename, int defaultHeight, int defaultWidth)
+	
+	/**
+	 * Load a double array from a file on disc
+	 * If the file cannot be found a default array filled with -1's will be returned
+	 * @param filename The file to load
+	 * @param size The size of the array to load
+	 * @return The double array from the specified file or an array of -1's with length size if file not found/invalid 
+	 */
+	public static double[] loadArray(String filename, int size)
 	{
-		
-		timer = System.currentTimeMillis();
-		
-		FileInputStream fis = null;
-		ObjectInputStream ois = null;
+		double[] data = new double[size];
+		ObjectInputStream inputStream = null;
 		try {
-			fis = new FileInputStream(filename);
-			ois = new ObjectInputStream(fis);
-			return (double[][]) ois.readObject();
-			
+			inputStream = new ObjectInputStream(new FileInputStream(filename));	
+			System.arraycopy(inputStream.readObject(), 0, data, 0, size);
+			inputStream.close();
 		} catch (Exception e) {
-			double[][] boardValues = new double[defaultHeight][];
-			for (int i = 0; i < defaultHeight; i++)
-			{
-				boardValues[i] = new double[]{-1,-1};
-			}
-			return boardValues;
+			Arrays.fill(data, -1);
 		}
-		finally{
-			System.out.println("Load array time: " + (System.currentTimeMillis() - timer));
-
-			
-			try {
-				ois.close();
-				fis.close();
-			} catch (Exception e) {}
-		}
+		return data;
 	}
 	
-	
-	public static double[] cacheFixFunction(String filename)
-	{
-		
-		timer = System.currentTimeMillis();
-		
-		double[][] readArray = null;
-		
-		
-		FileInputStream fis = null;
-		ObjectInputStream ois = null;
-		try {
-			fis = new FileInputStream(filename);
-			ois = new ObjectInputStream(fis);
-			readArray = (double[][]) ois.readObject();
-			
-			double[] fixedArray = new double[2000000];
-			
-			for (int i = 0; i < readArray.length; i++) {
-				fixedArray[i*2] = readArray[i][0];
-				fixedArray[i*2 + 1] = readArray[i][1];
-			}
-			
-			return fixedArray;
-			
-		} catch (Exception e) {
-			return null;
-		}
-		finally{
-			
-			try {
-				ois.close();
-				fis.close();
-			} catch (Exception e) {}
-		}
-	}
-	
-	public static void storeDoubleArray(double[][] data, String filename)
-	{
-		store(data,filename);
-	}
-	
-	static long timer = 0;
-	
-	public static double[] loadArray(String filename, int defaultSize)
-	{
-		timer = System.currentTimeMillis();
-		
-		FileInputStream fis = null;
-		ObjectInputStream ois = null;
-		try {
-			fis = new FileInputStream(filename);
-			ois = new ObjectInputStream(fis);
-			double[] data = new double[defaultSize];
-			System.arraycopy(ois.readObject(), 0, data, 0, defaultSize);
-			return data;
-			
-		} catch (Exception e) {
-			double[] boardValues = new double[defaultSize];
-			Arrays.fill(boardValues, -1);
-			return boardValues;
-		}
-		finally{
-			System.out.println("Load array time: " + (System.currentTimeMillis() - timer));
-			
-			try {
-				ois.close();
-				fis.close();
-				
-				
-			} catch (Exception e) {}
-		}
-		
-		
-	}
-	
-	//Save lookup table to persistent medium
+	/**
+	 * Store a double array to file on disc
+	 * @param data The data to write to disc
+	 * @param filename The name of the file to write
+	 */
 	public static void storeArray(double[] data, String filename)
 	{
-		store(data,filename);
-	}
-	
-	private static void store(Object data, String filename){
 		try {
-			FileOutputStream fos = new FileOutputStream(filename);
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			oos.writeObject(data);
-			oos.close();
-			fos.close();
+			ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filename));
+			outputStream.writeObject(data);
+			outputStream.close();
 		} catch (IOException e) {
-			System.out.println("WARNING! cache not stored");
+			System.out.println("WARNING! Cache might not be stored!");
 		}
 	}
 	
-	
-	
-	public static void main(String[] args) {
-		
-		for (int i = 0; i < 11; i++) {
-			
-			double[] d = cacheFixFunction("multiPlayerCache" + i + ".bin");
-			
-			store(d, "multiPlayerCacheIMPROV" + i + ".bin");
-			
-			
-			System.out.println("done for aggro " + i);
-		}
-	}
 }
