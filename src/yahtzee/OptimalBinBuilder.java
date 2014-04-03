cf7137fec24a01af25285cd74973fc8a6fafd507
Rasmus Greve <rasmusgreve@gmail.com>
2014-04-03 11:29:25 +0200

    Optimal bin builder now building bins

--- src/yahtzee/OptimalBinBuilder.java
+++ src/yahtzee/OptimalBinBuilder.java
@@ -9,14 +9,14 @@ public class OptimalBinBuilder {
 	public static void main(String[] args) {
 		OptimalMultiPlayerAI ai = new OptimalMultiPlayerAI();
 		
-		Scoreboard mysb = new Scoreboard(10, 3);
-		Scoreboard opponentsb = new Scoreboard(10, 3);
+		Scoreboard mysb = new Scoreboard(11, 2);
+		Scoreboard opponentsb = new Scoreboard(11, 2);
 		
 		int state = State.convertScoreboardsToState(mysb, opponentsb);
 		
 		mysb.PrintScoreBoard();
 		
 		System.out.println("ai.getStateValue(state, true): " + ai.getStateValue(state, true));
-		
+		ai.cleanUp();
 	}
 }
